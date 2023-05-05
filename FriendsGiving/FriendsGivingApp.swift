d4cb3a3ee6a710d2d9be6ee6e43e975d158d6ed6
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-05-05 13:17:45 +0200

    Initial Commit

---
+++ FriendsGiving/FriendsGivingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsGivingApp.swift
+//  FriendsGiving
+//
+//  Created by Eric on 05/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct FriendsGivingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
